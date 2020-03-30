74705ca3044d7c3539ae04a9e90fac04ca0de02b
tiffanyannhuey <87tiffanyhuey@gmail.com>
2020-03-29 17:49:41 -0700

    Add twitter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TwitterSearchComponent } from './twitter-search/twitter-search.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TwitterSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/twitter-search/twitter-search.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-twitter-search',
+  templateUrl: './twitter-search.component.html',
+  styleUrls: ['./twitter-search.component.scss']
+})
+export class TwitterSearchComponent implements OnInit {
+
+  twitterForm: FormGroup;
+
+  constructor(
+    private _formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.twitterForm = this._formBuilder.group({
+      query: ''
+    })
+  }
+
+}
